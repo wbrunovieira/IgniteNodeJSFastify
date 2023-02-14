fbaf0c4bdeeabccd43359ff70fda31e30febe5a3
wbrunovieira <wbrunovieira77@gmail.com>
2023-02-14 20:32:43 -0300

    insert a transactio to database

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transacao de teste',
+      amount: 10000,
+    })
+    .returning('*')
 
-  return tables
+  return transactions
 })
 
 app
