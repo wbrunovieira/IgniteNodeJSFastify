a50fc24c707a3570944cd0529d92cdb3378d2ea3
wbrunovieira <wbrunovieira77@gmail.com>
2023-02-18 12:37:42 -0300

    created the route tho get the transaction by id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,11 +5,23 @@ import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
   app.get('/', async () => {
-    const transactions = await knex('transactions')
-      .where('amount', 1000)
-      .select('*')
+    const transactions = await knex('transactions').select('*')
 
-    return transactions
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
   })
 
   app.post('/', async (request, reply) => {
