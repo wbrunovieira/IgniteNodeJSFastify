5788d92cdf7fe3a4ff3b18473425b25558695c85
wbrunovieira <wbrunovieira77@gmail.com>
2023-02-18 12:54:04 -0300

    added cookie for sessions

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
 import { env } from './env'
+import cookie from '@fastify/cookie'
 
 import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
+
+app.register(cookie)
 
 app.register(transactionsRoutes, {
   prefix: 'transactions',
